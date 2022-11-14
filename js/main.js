b900931b7a10dfaaba3833b5d5665b4426063772
Артур Олмасов <olmasovarthur@yandex.ru>
2022-11-14 20:57:35 +0500

    создаёт массив из 10 объектов

--- js/main.js
+++ js/main.js
@@ -25,77 +25,80 @@ const getRandomArrayElement = (elements) => elements[
   getRandomPositiveInteger(0, elements.length - 1)
 ];
 
-/*
-1) Итоговый результат: массив из 10-ти объявлений.
-2) Каждое объявление - тоже массив, который состоит из переменных:
-    autor, offer и location.
-*/
 
-const autor = {
-  avatar,
+const createAuthor = () => {
+  const authorId = getRandomPositiveInteger(1, 10);
+  return {
+    avatar: `img/avatars/user${authorId < 10 ? '0' : ''}${authorId}.png`
+  }
 }
 
-    const createAutor = () => {
-      let x = getRandomPositiveInteger(1, 10);
-      if (x < 10) {
-        return autor.avatar = 'img/avatars/user' + '0' + x + '.png';
-      } else {
-      return autor.avatar = 'img/avatars/user' + x + '.png';
-      }
-    }
 
-const offer = {
-  title: 'Заголовок предложения',
-  address,
-  price,
-  type: [`palace`, `flat`, `house`, `bungalow`, `hotel`],
-  rooms,
-  guests,
-  checkin: ['12:00', '13:00', '14:00'],
-  checkout: ['12:00', '13:00', '14:00'],
-  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elewator', 'conditioner'],
-  description: 'описание помещения',
-  photos: [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ],
+const randomArray = (array) => {
+  let randomLength = getRandomPositiveInteger(1, array.length);
+  let randomElement = getRandomPositiveInteger(1, randomLength);
+  let result = [];
+
+  for (let i = randomElement; i <= randomLength; i++) {
+      result.push(array[i])
+  }
+
+  return array;
 }
 
-    const createOffer = () => {
-      offer.address = [location.lat, location.lng]
-      offer.price = getRandomPositiveInteger(100, 500) + '$';
-      offer.type = getRandomArrayElement(offer.type);
-      offer.rooms = getRandomPositiveInteger(1, 5);
-      offer.guests = getRandomPositiveInteger(1, 5);
-      offer.checkin = getRandomArrayElement(offer.checkin);
-      offer.checkout = getRandomArrayElement(offer.checkout);
 
-      let getFeatures = function() {
-        for (let i = getRandomPositiveInteger(1, offer.features.length - 1); i <= offer.features.length - 1; i++) {
-          offer.features += offer.features[i];
-        }
-        return offer.features;
-      }
-
-      let getPhotos = function() {
-        for (let i = getRandomPositiveInteger(1, offer.photos.length - 1); i <= offer.photos.length - 1; i++) {
-          offer.photos += offer.photos[i];
-        }
-        return offer.photos;
-      }
-
+const createOffer = () => {
+  let type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+  let checkin = ['12:00', '13:00', '14:00'];
+  let checkout = ['12:00', '13:00', '14:00'];
+  let features = ['wifi', 'dishwasher', 'parking', 'washer', 'elewator', 'conditioner'];
+  let photos = [
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+      ];
+  const getAddress = createLocation();
+  const getPrice = getRandomPositiveInteger(100, 500);
+  const getType = randomArray(type);
+  const getRooms = getRandomPositiveInteger(1, 5) + ' rooms';
+  const getGuests = getRandomPositiveInteger(1, 5) + ' guests';
+  const getCheckin = getRandomArrayElement(checkin);
+  const getCheckout = getRandomArrayElement(checkout);
+  const getFeatures = randomArray(features);
+  const getPhotos = randomArray(photos);
+    return {
+      title: 'Заголовок предложения',
+      address: getAddress,
+      price: getPrice + '$',
+      type: getType,
+      rooms: getRooms,
+      guests: getGuests,
+      checkin: getCheckin,
+      checkout: getCheckout,
+      features: getFeatures,
+      description: 'описание помещения',
+      photos: getPhotos,
     }
+  }
 
-const location = {
-  lat: getRandomArbitrary(35.65000, 35.70000, 5),
-  lng: getRandomArbitrary(139.70000, 139.80000, 5),
+const createLocation = () => {
+  lat = getRandomArbitrary(35.65000, 35.70000, 5);
+  lng = getRandomArbitrary(139.70000, 139.80000, 5);
+  return {lat, lng};
 }
 
 const createDescription = () => ({
-  autor: createAutor(),
+  autor: createAuthor(),
   offer: createOffer(),
-  location: [location.lat, location.lng],
+  location: createLocation(),
 });
 
-const similarDescriptions = Array.from({length: 10}, createDescription);
+const createBigMassive = () => {
+  let bigMassive = [];
+  for (let i = 1; i <= 10; i++) {
+    bigMassive.push(createDescription());
+  }
+  return bigMassive;
+}
+
+createBigMassive();
